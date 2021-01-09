be9df813f8d9e6e9aaeba7e5b5414d50231c12d8
Nguyen Manh Linh <nmlinh2799@gmail.com>
2021-01-09 20:29:16 +0700

    series-posting & series posted views

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
 // const passport = require('passport');
 // const flash = require('connect-flash');
 // const session = require('express-session');
@@ -28,6 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
